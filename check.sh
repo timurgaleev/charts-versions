21feb9838ec1e2399bf9bd7130f5ce40756e899b
timurgaleev <timur_galeev@outlook.com>
2023-07-19 08:31:23 +0200

    Fixed push command

--- check.sh
+++ check.sh
@@ -3,79 +3,78 @@
 USERNAME=${GITHUB_ACTOR}
 REPONAME=$(echo "${REPOSITORY}" | cut -d'/' -f2)
 
-SHELL_DIR=$(dirname $0)
+DIR_NAME=$(dirname $0)
 
 REPOSITORY="timurgaleev/charts-versions"
 
-CHARTS=${SHELL_DIR}/target/charts.txt
-
-_verify_version() {
-    CHART="$1"
-    # CHART_URL="$2"
-
-    REPO="$(echo ${CHART} | cut -d'/' -f1)"
-    NAME="$(echo ${CHART} | cut -d'/' -f2)"
-
-    touch ${SHELL_DIR}/versions/${NAME}
-    NOW="$(cat ${SHELL_DIR}/versions/${NAME} | xargs)"
-
-    NEW="$(cat ${CHARTS} | grep "\"${CHART}\"" | awk '{print $2" ("$3")"}' | xargs)"
-
-    printf '# %-50s %-20s %-20s\n' "${CHART}" "${NOW}" "${NEW}"
-
-    printf "${NEW}" > ${SHELL_DIR}/versions/${NAME}
-
-    if [ "${NOW}" == "${NEW}" ]; then
-        return
-    fi
-
-    echo
-}
+CHARTS=${DIR_NAME}/target/charts.txt
 
 _init() {
-    rm -rf ${SHELL_DIR}/target
+  rm -rf ${DIR_NAME}/target
 
-    mkdir -p ${SHELL_DIR}/target
-    mkdir -p ${SHELL_DIR}/versions
+  mkdir -p ${DIR_NAME}/target
+  mkdir -p ${DIR_NAME}/versions
 
-    cp -rf ${SHELL_DIR}/versions ${SHELL_DIR}/target/
+  cp -rf ${DIR_NAME}/versions ${DIR_NAME}/target/
 }
 
-_load() {
-    helm version --client --short
+_search() {
+  helm version --client --short
 
-    # add repos
-    while read VAR; do
-        helm repo add ${VAR}
-    done < ${SHELL_DIR}/repos.txt
-    echo
+  while read LINE; do
+    helm repo add ${LINE}
+  done <${DIR_NAME}/repos.txt
+  echo
 
-    helm search repo -o json | jq '.[] | "\"\(.name)\" \(.version) \(.app_version)"' -r > ${CHARTS}
+  helm search repo -o json | jq '.[] | "\"\(.name)\" \(.version) \(.app_version)"' -r >${CHARTS}
 }
 
 _check() {
-    printf '# %-50s %-20s %-20s\n' "NAME" "NOW" "NEW"
+  printf '# %-50s %-20s %-20s\n' "NAME" "NOW" "NEW"
 
-    # verify versions
-    while read VAR; do
-        _verify_version ${VAR}
-    done < ${SHELL_DIR}/checklist.txt
-    echo
+  while read LINE; do
+    _check_version ${LINE}
+  done <${DIR_NAME}/checklist.txt
+  echo
+}
+
+_check_version() {
+  CHART="$1"
+  CHART_URL="$2"
+
+  REPO="$(echo ${CHART} | cut -d'/' -f1)"
+  NAME="$(echo ${CHART} | cut -d'/' -f2)"
+
+  touch ${DIR_NAME}/versions/${NAME}
+  NOW="$(cat ${DIR_NAME}/versions/${NAME} | xargs)"
+
+  NEW="$(cat ${CHARTS} | grep "\"${CHART}\"" | awk '{print $2" ("$3")"}' | xargs)"
+
+  printf '# %-50s %-20s %-20s\n' "${CHART}" "${NOW}" "${NEW}"
+
+  printf "${NEW}" >${DIR_NAME}/versions/${NAME}
+
+  if [ "${NOW}" == "${NEW}" ]; then
+    return
+  fi
+
+  if [ -z "${SLACK_TOKEN}" ]; then
+    return
+  fi
 }
 
 _message() {
-    # commit message
-    printf "$(date +%Y%m%d-%H%M)" > ${SHELL_DIR}/target/commit_message.txt
+  printf "$(date +%Y%m%d-%H%M)" >${DIR_NAME}/target/message.txt
 }
 
 _run() {
-    _init
+  _init
 
-    _load
+  _search
 
-    _check
+  _check
 
-    _message
+  _message
 }
 
 _run
